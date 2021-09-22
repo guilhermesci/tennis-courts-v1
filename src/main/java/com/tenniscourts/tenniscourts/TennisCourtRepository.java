cd3b0c6a5753126c3db7115e0465dea43329743a
guilhermesci <guilherme.s.c.ibanez@gmail.com>
2021-09-21 20:35:21 -0400

    Refactor tennis court features

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtRepository.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtRepository.java
@@ -2,5 +2,10 @@ package com.tenniscourts.tenniscourts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TennisCourtRepository extends JpaRepository<TennisCourt, Long> {
+
+    Optional<TennisCourt> findByName(String name);
+
 }
--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtService.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtService.java
@@ -1,22 +1,32 @@
 package com.tenniscourts.tenniscourts;
 
+import com.tenniscourts.exceptions.AlreadyExistsEntityException;
 import com.tenniscourts.exceptions.EntityNotFoundException;
 import com.tenniscourts.schedules.ScheduleService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class TennisCourtService {
 
     private final TennisCourtRepository tennisCourtRepository;
-
     private final ScheduleService scheduleService;
-
     private final TennisCourtMapper tennisCourtMapper;
 
     public TennisCourtDTO addTennisCourt(TennisCourtDTO tennisCourt) {
+        verifyIfTennisCourtExistsWithGivenName(tennisCourt.getName());
+
         return tennisCourtMapper.map(tennisCourtRepository.saveAndFlush(tennisCourtMapper.map(tennisCourt)));
+    }
+
+    private void verifyIfTennisCourtExistsWithGivenName(String name) {
+        Optional<TennisCourt> optTennisCourt = tennisCourtRepository.findByName(name);
+        if ( optTennisCourt.isPresent() ){
+            throw new AlreadyExistsEntityException(String.format("Tennis Court %s already exists",name));
+        }
     }
 
     public TennisCourtDTO findTennisCourtById(Long id) {
@@ -28,6 +38,7 @@ public class TennisCourtService {
     public TennisCourtDTO findTennisCourtWithSchedulesById(Long tennisCourtId) {
         TennisCourtDTO tennisCourtDTO = findTennisCourtById(tennisCourtId);
         tennisCourtDTO.setTennisCourtSchedules(scheduleService.findSchedulesByTennisCourtId(tennisCourtId));
+
         return tennisCourtDTO;
     }
 }
